95ed3da48509abea127e729dc2787d42abee53a4
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-09-02 14:56:55 +0300

    updated files

---
+++ src/components/AnimRoutes.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const AnimRoutes = () => {
+    return <div>AnimRoutes</div>;
+
+};
+
+export default AnimRoutes;
---
+++ src/context/CursorContext.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const CursorContext = () => {
+    return <div>CursorContext</div>;
+
+};
+
+export default CursorContext;
