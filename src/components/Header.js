cc78a76b96d41bb32ea7f201efeb95400233f0fa
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-10-17 15:04:14 +0300

    edited files

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,14 +17,47 @@ const Header = () => {
     return <header className="bg-pink-200 fixed w-full 
     px-[30px] lg:px-[100px]
     z-30 h-[100px] lg:h-[140px] flex items-center">
-        <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+        <div className="flex flex-col lg:flex-row 
+        lg:items-center w-full justify-between">
             {/* Logo */}
-            <link>
-              <img src="" alt=""/>
-            </link>
+            <Link to={'/'} className='max-w-[200px]'>
+              <img src="logo" alt=""/>
+            </Link>
+
+            <nav className="hidden xl:flex gap-x-12 font-semibold"> 
+               <Link 
+                  to={'/'} 
+                  className='text-[#696c6d] hover:text-primary transition'
+                  >Home
+               </Link>
+               <Link 
+                  to={'/about'} 
+                  className='text-[#696c6d] hover:text-primary transition'
+                  >About
+               </Link>
+               <Link 
+                  to={'/portfolio'} 
+                  className='text-[#696c6d] hover:text-primary transition'
+                  >Portfolio
+               </Link>
+               <Link 
+                  to={'/contact'} 
+                  className='text-[#696c6d] hover:text-primary transition'
+                  >Contact
+               </Link>
+            </nav>
+
+            {/*socials */}
+             <Socials />
+
+             {/*mobile nav */}
+             <MobileNav />
         </div>
-        </header>;
+        </header>
+    
+    
 
 };
 
 export default Header;
+
--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
-const MobileNav = () => {
-    return <div>MobileNav</div>;
+//import icons
 
+import {IoMdClose} from 'react-icons/io';
+import {CgMenuRight} from 'react-icons/cg';
+
+//import link
+import Link from 'react-router-dom';
+
+//import motion
+import {motion} from 'framer-motion'
+
+//menu variants
+const menuVariants = {
+    hidden: {
+        x: '100%'
+    },
+    show: {
+        x: 0,
+        transition: {
+            ease: [0.6, 0.01, -0.05, 0.9],
+        },
+    },
 };
 
-export default MobileNav;
