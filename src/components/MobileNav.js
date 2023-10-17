c87b8b3ba66667e2aa8a9060619dfe62fe445b68
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-10-17 15:05:02 +0300

    updated files

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -24,3 +24,50 @@ const menuVariants = {
     },
 };
 
+const MobileNav = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+    return (
+      <nav className="text-primary xl:hidden">
+
+        {/*nav open button */}
+        <div onClick={() => setOpenMenu(true)} className="text-3xl cursor pointer">
+            <CgMenuRight/>
+        </div>
+
+        {/*menu */}
+        <motion.div 
+        variants={menuVariants}
+        initial='hidden'
+        animate={openMenu ? 'show' : ''}
+        className="bg-white shadow-2xl w-full absolute 
+        top-0 right-0 max-w-xs h-screen z-20">
+
+            {/*icon */} 
+            <div onClick={() => setOpenMenu(false)} className="text-4xl absolute z-30 left-4 top-14
+            text-primary cursor-pointer">
+                <IoMdClose/>
+            </div>
+
+            {/*menu list */}
+            <ul className="h-full flex flex-col justify-center items-center
+            items-center gap-y-8 text-primary font-primary font-bold text-3xl">
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>  
+                <li>
+                    <Link to='/about'>About</Link>
+                </li> 
+                <li>
+                    <Link to='/portfolio'>Portfolio</Link>
+                </li> 
+                <li>
+                    <Link to='/contact'>Contact</Link>
+                </li> 
+            </ul>
+        </div>
+    </nav>
+    );
+    
+};
+
+export default MobileNav;
